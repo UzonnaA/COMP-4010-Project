71c1602a049b6a29576c68fd384acc7bc6729015
Uzonna Alexander <70288485+UzonnaA@users.noreply.github.com>
2024-11-09 13:03:16 -0500

    Added growing farmland

--- enviro.py
+++ enviro.py
@@ -19,7 +19,11 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)      # Open space
 RED = (255, 0, 0)        # Fences
 BLUE = (0, 0, 255)       # Player
-BROWN = (139, 69, 19)    # Farmland
+BROWN = (139, 69, 19)    # Farmland Stage 1
+DARKGOLDENROD = (160, 90, 23)    # Farmland Stage 2
+COPPER = (191, 123, 42)    # Farmland Stage 3
+TIGERSEYE = (222, 157, 0)    # Farmland Stage 4
+YELLOW = (255, 215, 0)    # Farmland Stage 5
 PURPLE = (160, 32, 240)  # Enemy
 
 # Define cell types
@@ -66,7 +70,17 @@ def draw_grid(screen, grid):
             rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             # Determine cell color based on grid type, with priority to farmland
             if grid[y][x] == FARMLAND:
-                color = BROWN
+                cur_stage = farm_dict.get((y,x))
+                if cur_stage == 1:
+                    color = BROWN
+                elif cur_stage == 2:
+                    color = DARKGOLDENROD
+                elif cur_stage == 3:
+                    color = COPPER
+                elif cur_stage == 4:
+                    color = TIGERSEYE
+                elif cur_stage == 5:
+                    color = YELLOW
             elif grid[y][x] == PLAYER:
                 color = BLUE
             elif grid[y][x] == ENEMY:
@@ -100,6 +114,10 @@ running = True
 # Track the type of cell under the player to restore it when they move
 previous_cell_type = OPEN_SPACE
 
+# Farmland dictionary
+farm_dict = {}
+growth_tick = 0
+
 while running:
     clock.tick(FPS)
     
@@ -110,6 +128,13 @@ while running:
     # Player action with 10% chance to place farmland, 90% chance to move
     # This will be a reward system later
     action = random.choices(['move', 'farm'], weights=[90, 10])[0]
+
+    if growth_tick >= 25:
+        for crop in farm_dict.keys():
+            if farm_dict[crop] < 5:
+                farm_dict[crop] += 1
+                growth_tick = 0
+
     
     if action == 'move':
         # Update the previous cell to the stored type (either OPEN_SPACE or FARMLAND)
@@ -123,16 +148,28 @@ while running:
         farmland_x, farmland_y = player_pos[0], player_pos[1] + 1
         if farmland_y < GRID_HEIGHT and grid[farmland_y][farmland_x] == OPEN_SPACE:
             grid[farmland_y][farmland_x] = FARMLAND
+            farm_dict[(farmland_y,farmland_x)] = 1
             print("Placed farmland at:", (farmland_x, farmland_y))  # Debug statement to confirm farmland placement
+
 
     # Set the player's current position
     grid[player_pos[1]][player_pos[0]] = PLAYER
 
+    # The player took the action
+    growth_tick += 1
+
     # Move the enemy randomly
     new_enemy_pos = random_move(enemy_pos)
-    grid[enemy_pos[1]][enemy_pos[0]] = OPEN_SPACE  # Clear old enemy position
-    enemy_pos = new_enemy_pos
-    grid[enemy_pos[1]][enemy_pos[0]] = ENEMY  # Update new enemy position
+
+    ##! If the enemy destroys a crop, it cannot grow anymore
+    if (enemy_pos[1],enemy_pos[0]) in farm_dict.keys():
+        farm_dict.pop((enemy_pos[1],enemy_pos[0]), None)
+
+    # The enemy can't clear the player
+    if grid[enemy_pos[1]][enemy_pos[0]] != PLAYER:
+        grid[enemy_pos[1]][enemy_pos[0]] = OPEN_SPACE  # Clear old enemy position
+        enemy_pos = new_enemy_pos
+        grid[enemy_pos[1]][enemy_pos[0]] = ENEMY  # Update new enemy position
     
     # Draw everything
     screen.fill(WHITE)
