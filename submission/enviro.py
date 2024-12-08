f2aca16d6aa7b7b58819d39d33f2fccbebd4469d
ManueLucas <m.lucas2603@gmail.com>
2024-12-08 18:45:34 -0500

    finalizing submission and experiment generation, updated readme

---
+++ submission/enviro.py
@@ -0,0 +1,87 @@
+import random
+from constants import *
+
+class farmEnvironment:
+    def __init__(self):
+        self.farmland = {}
+        self.growth_tick = 0
+
+    def grow_farmland(self):
+        # Progress the stages of growth 
+        for crop in self.farmland.keys():
+            if self.farmland[crop] < 5:
+                self.farmland[crop] += 1
+
+        return self.farmland
+
+    def setup(self):
+        self.farmland = {}
+        grid = [[OPEN_SPACE for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+
+        for x in range(GRID_WIDTH):
+            grid[0][x] = FENCE
+            grid[GRID_HEIGHT - 1][x] = FENCE
+
+        for y in range(GRID_HEIGHT):
+            grid[y][0] = FENCE
+            grid[y][GRID_WIDTH - 1] = FENCE
+
+        player_pos = [GRID_WIDTH // 2, GRID_HEIGHT // 2]
+        grid[player_pos[1]][player_pos[0]] = PLAYER
+
+        enemy_pos = [random.randint(1, GRID_WIDTH - 2), random.randint(1, GRID_HEIGHT - 2)]
+
+        while grid[enemy_pos[1]][enemy_pos[0]] != OPEN_SPACE:
+            enemy_pos = [random.randint(1, GRID_WIDTH - 2), random.randint(1, GRID_HEIGHT - 2)]
+
+        grid[enemy_pos[1]][enemy_pos[0]] = ENEMY
+
+        return grid, player_pos, enemy_pos
+
+    def playerAct(self, pos, action, grid):
+        newX, newY = pos[0], pos[1]
+        successfully_planted = False
+        
+        if action == 0 and pos[1] > 0: newY -= 1
+
+        elif action == 1 and pos[1] < GRID_HEIGHT - 1: newY += 1
+
+        elif action == 2 and pos[0] > 0: newX -= 1
+
+        elif action == 3 and pos[0] < GRID_WIDTH - 1: newX += 1
+
+        elif action == 4:
+            farmland_x, farmland_y = pos[0], pos[1] + 1
+
+            if farmland_y < GRID_HEIGHT and grid[farmland_y][farmland_x] == OPEN_SPACE:
+                grid[farmland_y][farmland_x] = FARMLAND
+                self.farmland[(farmland_y,farmland_x)] = 1
+                successfully_planted = True
+
+        elif action == 5:
+            farmland_x, farmland_y = pos[0], pos[1] + 1
+
+            # if farmland_y < GRID_HEIGHT and grid[farmland_y][farmland_x] == FARMLAND:
+            if (farmland_y, farmland_x) in self.farmland:
+                grid[farmland_y][farmland_x] = OPEN_SPACE
+                del self.farmland[(farmland_y,farmland_x)]
+            
+        if grid[newY][newX] != FENCE:
+            return [newX, newY], successfully_planted
+        return pos, successfully_planted
+
+    def enemyAct(self, pos, action, grid):
+        newX, newY = pos[0], pos[1]
+
+        if action == 0 and pos[1] > 0: newY -= 1
+
+        elif action == 1 and pos[1] < GRID_HEIGHT - 1: newY += 1
+
+        elif action == 2 and pos[0] > 0: newX -= 1
+
+        elif action == 3 and pos[0] < GRID_WIDTH - 1: newX += 1
+
+        if grid[newY][newX] != FENCE:
+            return [newX, newY]
+        
+        return pos
---
+++ submission/run.py
@@ -0,0 +1,76 @@
+import pygame
+import sys
+import numpy as np
+from constants import *
+from enviro import farmEnvironment
+from train import stepWithoutPenalty as step
+
+
+# Pygame setup
+def run(player, enemy, env):
+    env = farmEnvironment()
+
+    pygame.init()
+    print("STARTING...")
+
+    # Set up the display
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("2D Grid Environment with Farmland and Enemy")
+    clock = pygame.time.Clock()
+
+    # Main loop
+    running = True
+    # Track the type of cell under the player to restore it when they move
+    previous_cell_type = OPEN_SPACE
+
+    grid, player_pos, enemy_pos = env.setup()
+    
+
+    while running:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Use the updated step function that is now in train.py
+        playerAction, logprob_playerAction = player.select_action(np.array(grid).flatten(), deterministic=False) # use stochastic when training
+        #enemyAction, logprob_enemyAction = enemy.select_action(s, deterministic=False) # use stochastic when training
+        enemyAction = np.random.randint(0, 4)
+        nextState, playerReward, enemyReward, grid, player_pos, enemy_pos, previous_cell_type, info = step(
+            env, grid, player_pos, enemy_pos, playerAction, enemyAction, previous_cell_type
+        )
+        # Draw everything
+        screen.fill(WHITE)
+        
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                if grid[y][x] == FARMLAND:
+                    cur_stage = env.farmland.get((y, x))
+                    if cur_stage == 1:
+                        color = BROWN
+                    elif cur_stage == 2:
+                        color = DARKGOLDENROD
+                    elif cur_stage == 3:
+                        color = COPPER
+                    elif cur_stage == 4:
+                        color = TIGERSEYE
+                    elif cur_stage == 5:
+                        color = YELLOW
+                elif grid[y][x] == PLAYER:
+                    color = BLUE
+                elif grid[y][x] == ENEMY:
+                    color = PURPLE
+                elif grid[y][x] == OPEN_SPACE:
+                    color = GREEN
+                elif grid[y][x] == FENCE:
+                    color = RED
+                pygame.draw.rect(screen, color, rect)
+                pygame.draw.rect(screen, BLACK, rect, 1)
+
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
